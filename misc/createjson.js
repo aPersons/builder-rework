3f680ecba5bef0437a04746b6a0aee828ce7673d
aPersons <paulorsmth@gmail.com>
2022-08-28 23:07:08 +0300

    first commit

---
+++ misc/createjson.js
@@ -0,0 +1,7 @@
+let rawList = []
+for (let cat of document.querySelectorAll(".builder-part-category")) {
+  let objList = [];
+  let prodList = cat.querySelectorAll(".part-rd-bt, .part-checkbox");
+
+}
+console.log(JSON.stringify(rawList));
