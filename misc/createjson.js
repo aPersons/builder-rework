69d1d0608ef47925c0d6d0200b8543f897c24d21
aPersons <paulorsmth@gmail.com>
2022-09-05 11:50:24 +0300

    prod json and img refresh, removed duplicates

--- misc/createjson.js
+++ misc/createjson.js
@@ -1,49 +1,10 @@
-// let rawList = []
-// for (let cat of document.querySelectorAll(".builder-part-category")) {
-//   let prodList = cat.querySelectorAll(".part-rd-bt, .part-checkbox");
-
-//   let qCat = {
-//     "cat-code": cat.id,
-//     "cat-name": cat.firstElementChild.textContent,
-//     "cat-desc": "Το σπίτι του νέου σας συστήματος. Επιλέξτε το μοντέλο και το χρώμα που προτιμάτε!",
-//     "type": "$blank",
-//     "init-prod": "$blank",
-//     "singleQuant": false,
-//     "product-list": []
-//   } 
-  
-  
-//   console.log(prodList.length);
-//   for (let i=0;i<prodList.length&&i<12;i++) {
-//     qCat["product-list"].push({
-//       "prod-code": prodList[i].id,
-//       "prod-name": prodList[i].nextElementSibling.querySelector(".part-text-head").innerHTML,
-//       "prod-erp": prodList[i].dataset.erp,
-//       "prod-price": prodList[i].value,
-//     })
-
-//     if (prodList[i].nextElementSibling.querySelector(".part-number-input")) {
-//       let tmpQ = prodList[i].nextElementSibling.querySelector(".part-number-input")
-
-//       if (tmpQ.classList.contains("static-number")) {
-//         qCat["product-list"][i]["static-num"] = tmpQ.querySelector(".part-quantity").value;
-//       } else {
-//         qCat["product-list"][i]["prod-min"] = tmpQ.querySelector(".part-quantity").min;
-//         qCat["product-list"][i]["prod-max"] = tmpQ.querySelector(".part-quantity").max;
-//       }
-//     } else {
-//       qCat["product-list"][i]["static-num"] = "1";
-//     }
-//   }
-
-
-//   rawList.push(qCat);
-// }
-// console.log(JSON.stringify(rawList));
-
-
-
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@
+function rDuplicates(ar) {
+  let uAr = []
+  for (let itm of ar) {
+    if (!uAr.includes(itm)) uAr.push(itm);
+  }
+  return uAr
+}
 
 
 
@@ -61,12 +22,13 @@ for (let cnm of domCashe.domOrder) {
     "product-list": []
   } 
   
-  
-  for (let i=0;i<cat.prodOrder.length&&i<12;i++) {
-    let prod = cat.prodList[cat.prodOrder[i]];
+  let uOrder = rDuplicates(cat.prodOrder);
+
+  for (let i=0;i<uOrder.length&&i<12;i++) {
+    let prod = cat.prodList[uOrder[i]];
   
     qCat["product-list"].push({
-      "prod-code": cat.prodOrder[i],
+      "prod-code": uOrder[i],
       "prod-name": prod.nmTxt.innerHTML,
       "prod-erp": prod.erp,
       "prod-price": prod.priceVal.toString(),
--- misc/fechIMG.js
+++ misc/fechIMG.js
@@ -1,24 +1,41 @@
-let total = 0;
-for (let cnm of domCashe.domOrder) {
-  let cat = domCashe.dom[cnm]
+function rDuplicates(ar) {
+  let uAr = []
+  for (let itm of ar) {
+    if (!uAr.includes(itm)) uAr.push(itm)
+  }
+  return uAr
+}
 
-  for (let i=0;i<cat.prodOrder.length&&i<12;i++) {
-    let cnm = cat.prodOrder[i];
+
+let total = 0;
+let dl = 0;
+for (let cnm of domCashe.domOrder) {
+  let cat = domCashe.dom[cnm];
+
+  let uOrder = rDuplicates(cat.prodOrder);
+
+  for (let i=0;i<uOrder.length&&i<12;i++) {
+    let ldl = dl;
+    dl++
+    let cnm = uOrder[i];
     let prod = cat.prodList[cnm];
-    fetch(prod.cDom.querySelector(".build-img").src.replace("sys_thumbs","cat_thumbs")).then(function(response) {
-      return response.blob()
-    }).then(function(blb){
-      let nEl = document.createElement("template");
-      nEl.innerHTML = 
-        `<a href="${URL.createObjectURL(blb)}"
-            download="${cnm}"
-            class="d-none imgdownload"></a>`
-      ;
-      nEl = nEl.content.firstElementChild;
-      // console.log(nEl);
-      document.body.insertAdjacentElement("beforeend", nEl);
-      nEl.click();
-    })   
+    setTimeout(()=>{
+      fetch(prod.cDom.querySelector(".build-img").src.replace("sys_thumbs","cat_thumbs")).then(function(response) {
+        return response.blob()
+      }).then(function(blb){
+        let nEl = document.createElement("template");
+        nEl.innerHTML = 
+          `<a href="${URL.createObjectURL(blb)}"
+              download="${cnm}"
+              class="d-none imgdownload"></a>`
+        ;
+        nEl = nEl.content.firstElementChild;
+        // console.log(nEl);
+        document.body.insertAdjacentElement("beforeend", nEl);
+        nEl.click();
+      }) 
+    },ldl*1000);
+      
     total++
     // break;
   }
