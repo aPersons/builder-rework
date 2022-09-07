9a193ed5b3d6588ea4b115996d1af211e11be626
aPersons <paulorsmth@gmail.com>
2022-09-07 18:01:14 +0300

    markup and css

--- misc/create-markup.py
+++ misc/create-markup.py
@@ -1,20 +1,85 @@
 import json
 
+def crNumberInput(cat, prod):
+  res = ""
+
+  if "static-num" in prod:
+    res += """
+<div class="part-number-input static-number">
+  <input type="number" class="part-quantity" id="{prod_code}_qty" name="{cat_code}_qty" value="{static_quant}" readonly>
+</div>""".format(
+      prod_code = prod["prod-code"],
+      cat_code = cat["cat-code"],
+      static_quant = prod["static-num"],
+    )
+  else:
+    res += """
+<div class="part-number-input">
+  <input type="number" class="part-quantity" id="{prod_code}_qty" name="{cat_code}_qty" min="{min}" max="{max}" value="0">
+  <i class="bi bi-dash part-num-decr"></i>
+  <span class="quantity-display">0</span>
+  <i class="bi bi-plus part-num-incr"></i>
+</div>""".format(
+      prod_code = prod["prod-code"],
+      cat_code = cat["cat-code"],
+      min = prod["prod-min"],
+      max = prod["prod-max"],
+    )  
+
+  return res
+
+def crProdList(cat):
+  res = ""
+  for prod in cat["product-list"]:
+
+    prodStr = """
+<input type="radio" class="part-rd-bt" id="{prod_code}" name="{cat_code}" value="{prod_code}"{prod_erp}>
+<label class="listed-part" for="{prod_code}">
+  <div class="part-img">
+    <img class="build-img" src="img/{prod_code}.jpg" width="100%">
+  </div>
+  <div class="part-text">
+    <div class="part-text-head fs-sm">
+      {prod_name}
+    </div>
+  </div>
+  {number_input}
+  <div class="prod-quick-view">
+    <a class="quick-view-btn fs-xs" data-productid="38615" href="#quick-view" data-bs-toggle="modal"><i class="bi bi-eye"></i></a>
+  </div>
+  <div class="part-price fw-bold" data-priceval="{prod_price}">
+    <span class="price-block">+0,00€</span>
+  </div>
+</label>""".format(
+      prod_code = prod["prod-code"],
+      cat_code = cat["cat-code"],
+      prod_erp = f' data-erp="{prod["prod-erp"]}"' if prod["prod-erp"] != "-" else "",
+      prod_name = prod["prod-name"],
+      number_input = crNumberInput(cat, prod),
+      prod_price = prod["prod-price"]
+    )
+    
+    res += prodStr
+
+  return res
+
 def crCat(cat):
   res = """
 <div class="builder-part-category" id="{catId}">
-  <div class="part-category-head">{catName}</div>
-  {description}   
-  <div class="part-list-container-outer">
-    <div class="part-list-container">
-      {prodlist}
-    </div>
+  <div class="part-category-header">
+    <div class="part-category-img"><img src="img/cat29-prod0.jpg"></div>
+    <div class="part-category-bar">{catName}</div>
+    <div class="part-category-details"></div>
+  </div>
+  {description}
+  <div class="part-list-container">
+    {prodlist}
   </div>
 </div>""".format(
     catId = cat["cat-code"],
     catName = cat["cat-name"],
     description = "" if cat["cat-desc"] == "$blank" else f'<div class="part-category-description fs-md bg-secondary">{cat["cat-desc"]}</div>',
-    prodlist = ""
+    prodlist = crProdList(cat)
   )
 
   return res
